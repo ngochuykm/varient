39fd30e8d0e3597cd9da77c26dc3dbbd0d997bf6
VU NGOC HUY <fromhd.1990@gmail.com>
2020-12-18 16:04:19 +0700

    ffdfd

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as page from './pages'
 import {
-    HashRouter, Route, Switch
+    BrowserRouter, Route, Switch
 } from 'react-router-dom'
 import {RouteItem} from '../src/route/RouteItem'
 let RouteItems = []
@@ -9,10 +9,10 @@ const RoutesItemRender = (pageItem,pageSource) => (
     pageItem.map((value,key) => {
         if(value.pageItem){
             console.log("/varient/#" + value.pageLink)
-            RouteItems.push(<Route key={key} exact path={"/varient/#" + value.pageLink} component={() => <value.component pageSource={pageSource} name={value.name} pageLink={value.pageLink} dataPage={value.dataPage}/>}/>)
+            RouteItems.push(<Route key={key} exact path={"/varient" + value.pageLink + "/"} component={() => <value.component pageSource={pageSource} name={value.name} pageLink={value.pageLink} dataPage={value.dataPage}/>}/>)
             RoutesItemRender(value.pageItem,pageSource.concat([{name:value.name, pageLink:value.pageLink}]))
         }else{
-            RouteItems.push(<Route key={key} exact path={"/varient/#" + value.pageLink} component={() => <value.component pageSource={pageSource} name={value.name} pageLink={value.pageLink} dataPage={value.dataPage}/>}/>)
+            RouteItems.push(<Route key={key} exact path={"/varient" + value.pageLink} component={() => <value.component pageSource={pageSource} name={value.name} pageLink={value.pageLink} dataPage={value.dataPage}/>}/>)
         }
     })
 )
@@ -20,11 +20,11 @@ const Routes = () => {
     RoutesItemRender(RouteItem,[])
     console.log(RouteItems)
     return(
-    <HashRouter>
+    <BrowserRouter>
         <Switch>
             {RouteItems}
-            <Route exact path="/hhh" component={page.HomePage}/>
+            <Route exact path="/jjh" component={page.HomePage}/>
         </Switch>
-    </HashRouter>
+    </BrowserRouter>
 )}
 export default Routes
