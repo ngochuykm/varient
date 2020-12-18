801f8ae5097c7a694135f6bc56f44e1d47b52922
VU NGOC HUY <fromhd.1990@gmail.com>
2020-12-18 16:18:23 +0700

    ffdfd

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import * as page from './pages'
 import {
-    BrowserRouter, Route, Switch
+    HashRouter, Route, Switch
 } from 'react-router-dom'
 import {RouteItem} from '../src/route/RouteItem'
 let RouteItems = []
 const RoutesItemRender = (pageItem,pageSource) => (
     pageItem.map((value,key) => {
         if(value.pageItem){
-            console.log("/varient/#" + value.pageLink)
-            RouteItems.push(<Route key={key} exact path={"/varient" + value.pageLink + "/"} component={() => <value.component pageSource={pageSource} name={value.name} pageLink={value.pageLink} dataPage={value.dataPage}/>}/>)
+            console.log("/varient" + value.pageLink)
+            RouteItems.push(<Route key={key} exact path={"/varient" + value.pageLink} component={() => <value.component pageSource={pageSource} name={value.name} pageLink={value.pageLink} dataPage={value.dataPage}/>}/>)
             RoutesItemRender(value.pageItem,pageSource.concat([{name:value.name, pageLink:value.pageLink}]))
         }else{
             RouteItems.push(<Route key={key} exact path={"/varient" + value.pageLink} component={() => <value.component pageSource={pageSource} name={value.name} pageLink={value.pageLink} dataPage={value.dataPage}/>}/>)
@@ -20,11 +20,11 @@ const Routes = () => {
     RoutesItemRender(RouteItem,[])
     console.log(RouteItems)
     return(
-    <BrowserRouter>
+    <HashRouter basename='/'>
         <Switch>
             {RouteItems}
             <Route exact path="/jjh" component={page.HomePage}/>
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 )}
 export default Routes
