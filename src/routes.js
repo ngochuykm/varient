3f6a9055a1eb42697cc601b3f445a031cda69c11
VU NGOC HUY <fromhd.1990@gmail.com>
2020-12-18 15:56:57 +0700

    ffdfd

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ let RouteItems = []
 const RoutesItemRender = (pageItem,pageSource) => (
     pageItem.map((value,key) => {
         if(value.pageItem){
-            console.log("/varient" + value.pageLink)
+            console.log("/varient/#" + value.pageLink)
             RouteItems.push(<Route key={key} exact path={"/varient/#" + value.pageLink} component={() => <value.component pageSource={pageSource} name={value.name} pageLink={value.pageLink} dataPage={value.dataPage}/>}/>)
             RoutesItemRender(value.pageItem,pageSource.concat([{name:value.name, pageLink:value.pageLink}]))
         }else{
