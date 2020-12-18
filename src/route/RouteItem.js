e136e7b66e6ec0b6be6809fb49d5f0e11ae46090
VU NGOC HUY <fromhd.1990@gmail.com>
2020-12-18 11:58:51 +0700

    ffdfd

--- src/route/RouteItem.js
+++ src/route/RouteItem.js
@@ -2,7 +2,7 @@ import *  as page from '../pages'
 import {HomePageItem} from './HomePageItem'
 export  const RouteItem = [
     {
-        pageLink:"/",
+        pageLink:"/varient",
         component: page.HomePage,
         name: "Home",
         pageItem:HomePageItem,
