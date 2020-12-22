1e9e21d556cb5a9bdcfa24964e89583cb5c4331d
VU NGOC HUY <fromhd.1990@gmail.com>
2020-12-23 01:54:17 +0700

    ffdfd

--- server.js
+++ server.js
@@ -48,3 +48,4 @@ var server = app.listen(process.env.PORT || 5000, function () {
     var port = server.address().port;
     console.log("Express is working on port " + port);
   });
+app.listen(process.env.PORT || 5000);
